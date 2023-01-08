d38d0cc8a207e48a70440c42b86d24b876e5a6a7
Denis Yusri Hyuanda <114201057+DenisYusri2101092013@users.noreply.github.com>
2023-01-08 18:52:16 +0700

    Add files via upload

---
+++ TEORI_PBO_2101092013/com/denis/p21092022/AritmatikaDemoDenis.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denis.p21092022;
+
+/**
+ *
+ * @author ACER
+ */
+public class AritmatikaDemoDenis {
+    public static void main(String[] args) {
+       int i = 37;
+       int j = 42;
+       double x = 27.475;
+       double y = 7.22;
+       
+        System.out.println("Variable values...");
+        System.out.println("    i = " + i);
+        System.out.println("    j = " + j);
+        System.out.println("    x = " + x);
+        
+        //penjumlahan angka
+        System.out.println("Adding...");
+        System.out.println("    i + j = " + (i + j));
+        System.out.println("    x + y = " + (x + y));
+        
+        //pengurangan angka
+        System.out.println("Substracting...");
+        System.out.println("    i - j = " + (i - j));
+        System.out.println("    x - y " + (x - y));
+
+        
+        //perkalian angka
+        System.out.println("Multiplying...");
+        System.out.println("    i * j = " + (i * j));
+        System.out.println("    x * y = " + (x * y));
+        
+        //pembagan angka
+        System.out.println("Dividing...");
+        System.out.println("    i / j = " + (i / j));
+        System.out.println("    x / y = " + (x / y));
+        
+        //Hitung hasil modulus dari pembagian
+        System.out.println("Computing the reminder......");
+        System.out.println("    i % j = " + (i % j));
+        System.out.println("    x % y = " + (x % y));
+        
+
+
+    }
+    
+}
